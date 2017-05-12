9c7e811eb5cf02a2072295d3c14ec5bf707162ac
Evgeny Rodionov <git@erodionov.ru>
2017-05-13 02:02:07 +0300

    chore: add eslint and prepush test

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+// eslint-disable react/jsx-filename-extension
+
+// eslint-disable react/jsx-filename-extension
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
